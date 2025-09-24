df6412f2cd47b6b5e075599cb42009e6f0867463
MichaelHu07 <mhu070706@gmail.com>
2025-09-23 22:48:12 -0400

    update

--- main.py
+++ main.py
@@ -119,8 +119,8 @@ if __name__ == '__main__':
 
             running_loss += loss.item()
 
-            if i % 2000 == 1999:  # print every 2000 mini-batches
-                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+            if i % 50 == 49:  # print every 50 mini-batches
+                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 50:.3f}')
                 running_loss = 0.0
 
     print("Finished training")
